76f95c4f299609ab6350ae7aad18a3bf1d4d689b
JairoCruz <azaveliz@gmail.com>
2015-09-28 09:37:20 -0600

    este commit es solo para utilizar las librerias que agregue en el anterior commit. su uso es mas facil trabajar con animaciones.

--- app/src/main/java/com/example/tse/listdirectorio/animate/AnimationUtils.java
+++ app/src/main/java/com/example/tse/listdirectorio/animate/AnimationUtils.java
@@ -4,6 +4,9 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.support.v7.widget.RecyclerView;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
 /**
  * Created by TSE on 25/09/2015.
  */
@@ -11,12 +14,19 @@ public class AnimationUtils {
 
     public static void animate(RecyclerView.ViewHolder holder, boolean goesDown){
 
-        AnimatorSet animatorSet = new AnimatorSet();
+        // Este codigo de animacion lo utilizo con librerias de terceros
+        YoYo.with(Techniques.Bounce.Wobble)
+                .duration(1000)
+                .playOn(holder.itemView);
+
+       /* AnimatorSet animatorSet = new AnimatorSet();
         ObjectAnimator animatorTranslateY = ObjectAnimator.ofFloat(holder.itemView, "translationY",goesDown == true ? 300: -300,0);
         ObjectAnimator animatorTranslateX = ObjectAnimator.ofFloat(holder.itemView, "translationX", -25, 25, -20, 20, -15, 15 - 10, 10, -5, 5 - 0);
         animatorTranslateY.setDuration(1000);
         animatorTranslateX.setDuration(1000);
         animatorSet.playTogether(animatorTranslateX,animatorTranslateY);
-        animatorSet.start();
+        animatorSet.start();*/
+
+
     }
 }
